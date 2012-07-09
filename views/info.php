ac63b3466767931c4ffbd3d9a82b2b1b4356e33a
renee morales2 <ing.renee.sis@gmail.com>
2012-07-09 10:33:20 -0500

    compracion de existencia variables formalizado

--- views/info.php
+++ views/info.php
@@ -9,7 +9,7 @@ $info=$this->native_session->userdata('login_data_candidatos');
 <table width="100%" border="0" cellpadding="0" cellspacing="0">
   <tr>
     <td width="17%"><a href="<?PHP echo site_url('imagen');?>">
-    <img src="<?PHP _imagen($info['ID']);?>"></a></td>
+    <img src="<?PHP _imagen($info['ID']);?>" class="avatar"></a></td>
     <td  valign="top">
     <div class="fila"><b>Nombres:</b>
 <?PHP echo $info['nombres'];?>,<?PHP echo $info['apellidos'];?>
