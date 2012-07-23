08906ce8630c43dd34aa494332ea39e1e8634a77
renee morales <ing.renee.sis@gmail.com>
2012-07-23 00:21:27 -0500

    Empezando a crear los archivos para la seccion de crear widgets

---
+++ views/widgets/index.php
@@ -0,0 +1,24 @@
+
+<div class="c1" style="width:40%; float:left">
+<div class="formulario">
+<div class="fila">
+<div class="caption">
+Tipo de widgets:
+</div>
+<label><input type="radio" name="tipo">
+Por categorias</label>
+<label><input type="radio" name="tipo">
+Por palabras clavess</label>
+
+</div>
+
+<div class="fila">
+
+</div>
+
+</div>
+
+</div>
+<div class="c1" style="width:58%; float:right">
+d55
+</div>
