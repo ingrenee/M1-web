43a9b5db14d7736ff6010d064c76e68129b1f608
renee morales <ing.renee.sis@gmail.com>
2012-07-24 21:40:15 -0500

    ..

---
+++ logs/log-2012-07-23.php
@@ -0,0 +1,19 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2012-07-23 01:08:50 --> Severity: Notice  --> Undefined index:  pais F:\wamp\www2\bolsa2011\sistema\application\web\views\welcome\lista.php 41
+ERROR - 2012-07-23 01:08:50 --> Severity: Notice  --> Undefined index:  pais F:\wamp\www2\bolsa2011\sistema\application\web\views\welcome\lista.php 41
+ERROR - 2012-07-23 01:08:50 --> Severity: Notice  --> Undefined index:  pais F:\wamp\www2\bolsa2011\sistema\application\web\views\welcome\lista.php 41
+ERROR - 2012-07-23 01:08:50 --> Severity: Notice  --> Undefined index:  pais F:\wamp\www2\bolsa2011\sistema\application\web\views\welcome\lista.php 41
+ERROR - 2012-07-23 01:08:50 --> Severity: Notice  --> Undefined index:  pais F:\wamp\www2\bolsa2011\sistema\application\web\views\welcome\lista.php 41
+ERROR - 2012-07-23 01:08:50 --> Severity: Notice  --> Undefined index:  pais F:\wamp\www2\bolsa2011\sistema\application\web\views\welcome\lista.php 41
+ERROR - 2012-07-23 01:08:50 --> Severity: Notice  --> Undefined index:  pais F:\wamp\www2\bolsa2011\sistema\application\web\views\welcome\lista.php 41
+ERROR - 2012-07-23 01:08:50 --> Severity: Notice  --> Undefined index:  pais F:\wamp\www2\bolsa2011\sistema\application\web\views\welcome\lista.php 41
+ERROR - 2012-07-23 01:08:50 --> Severity: Notice  --> Undefined index:  pais F:\wamp\www2\bolsa2011\sistema\application\web\views\welcome\lista.php 41
+ERROR - 2012-07-23 01:08:50 --> Severity: Notice  --> Undefined index:  pais F:\wamp\www2\bolsa2011\sistema\application\web\views\welcome\lista.php 41
+ERROR - 2012-07-23 01:08:50 --> Severity: Notice  --> Undefined variable: opciones F:\wamp\www2\bolsa2011\sistema\application\web\views\home.php 327
+ERROR - 2012-07-23 01:08:50 --> Severity: Notice  --> Undefined variable: opciones F:\wamp\www2\bolsa2011\sistema\application\web\views\home.php 442
+ERROR - 2012-07-23 01:08:50 --> Severity: Notice  --> Undefined variable: opciones F:\wamp\www2\bolsa2011\sistema\application\web\views\home.php 478
+ERROR - 2012-07-23 01:08:50 --> Severity: Notice  --> Undefined variable: opciones F:\wamp\www2\bolsa2011\sistema\application\web\views\home.php 496
+ERROR - 2012-07-23 01:12:53 --> Severity: Notice  --> Undefined variable: data F:\wamp\www2\bolsa2011\sistema\application\web\controllers\widgets.php 13
+ERROR - 2012-07-23 01:12:58 --> Severity: Notice  --> Undefined variable: data F:\wamp\www2\bolsa2011\sistema\application\web\controllers\widgets.php 13
+ERROR - 2012-07-23 01:22:01 --> Severity: Warning  --> session_destroy() [<a href='function.session-destroy'>function.session-destroy</a>]: Session object destruction failed F:\wamp\www2\bolsa2011\sistema\system\libraries\native_session.php 52
