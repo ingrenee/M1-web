bfa789142729bad974d2c34207e38191440f4fc0
renee morales2 <ing.renee.sis@gmail.com>
2012-07-25 11:29:55 -0500

    d

---
+++ logs/log-2012-07-23.php
@@ -0,0 +1,15 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2012-07-23 12:24:33 --> Severity: Notice  --> Undefined variable: content F:\wamp\www\Bolsa2011\sistema\application\web\views\template_solo.php 228
+ERROR - 2012-07-23 16:36:55 --> Severity: Notice  --> Undefined index:  categoria F:\wamp\www\Bolsa2011\sistema\application\web\controllers\widgets.php 93
+ERROR - 2012-07-23 16:36:57 --> Severity: Notice  --> Undefined index:  categoria F:\wamp\www\Bolsa2011\sistema\application\web\controllers\widgets.php 93
+ERROR - 2012-07-23 16:37:00 --> Severity: Notice  --> Undefined index:  categoria F:\wamp\www\Bolsa2011\sistema\application\web\controllers\widgets.php 93
+ERROR - 2012-07-23 16:37:02 --> Severity: Notice  --> Undefined index:  categoria F:\wamp\www\Bolsa2011\sistema\application\web\controllers\widgets.php 93
+ERROR - 2012-07-23 16:37:04 --> Severity: Notice  --> Undefined index:  categoria F:\wamp\www\Bolsa2011\sistema\application\web\controllers\widgets.php 93
+ERROR - 2012-07-23 16:37:23 --> Severity: Notice  --> Undefined index:  categoria F:\wamp\www\Bolsa2011\sistema\application\web\controllers\widgets.php 93
+ERROR - 2012-07-23 16:37:33 --> Severity: Notice  --> Undefined index:  categoria F:\wamp\www\Bolsa2011\sistema\application\web\controllers\widgets.php 93
+ERROR - 2012-07-23 16:37:35 --> Severity: Notice  --> Undefined index:  categoria F:\wamp\www\Bolsa2011\sistema\application\web\controllers\widgets.php 93
+ERROR - 2012-07-23 16:37:38 --> Severity: Notice  --> Undefined index:  categoria F:\wamp\www\Bolsa2011\sistema\application\web\controllers\widgets.php 93
+ERROR - 2012-07-23 16:37:53 --> Severity: Notice  --> Undefined index:  categoria F:\wamp\www\Bolsa2011\sistema\application\web\controllers\widgets.php 93
+ERROR - 2012-07-23 16:38:56 --> Severity: Notice  --> Undefined index:  categoria F:\wamp\www\Bolsa2011\sistema\application\web\controllers\widgets.php 93
+ERROR - 2012-07-23 16:38:59 --> Severity: Notice  --> Undefined index:  categoria F:\wamp\www\Bolsa2011\sistema\application\web\controllers\widgets.php 93
---
+++ logs/log-2012-07-24.php
@@ -0,0 +1,7 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2012-07-24 09:06:50 --> Severity: Notice  --> Undefined offset:  20 F:\wamp\www\Bolsa2011\sistema\application\web\controllers\widgets.php 137
+ERROR - 2012-07-24 10:01:33 --> Severity: Notice  --> Undefined offset:  150 F:\wamp\www\Bolsa2011\sistema\application\web\controllers\widgets.php 44
+ERROR - 2012-07-24 14:39:56 --> Severity: Notice  --> A session had already been started - ignoring session_start() F:\wamp\www\Bolsa2011\sistema\system\libraries\native_session.php 144
+ERROR - 2012-07-24 14:39:56 --> Severity: Notice  --> A session had already been started - ignoring session_start() F:\wamp\www\Bolsa2011\sistema\system\libraries\native_session.php 144
+ERROR - 2012-07-24 14:39:56 --> Severity: Notice  --> A session had already been started - ignoring session_start() F:\wamp\www\Bolsa2011\sistema\system\libraries\native_session.php 144
