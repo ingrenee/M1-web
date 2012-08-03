59e753cbfbfd97888144d7306230fbe0bac8c1d1
renee morales <ing.renee.sis@gmail.com>
2012-08-02 19:35:42 -0500

    basura

---
+++ logs/log-2012-07-25.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2012-07-25 00:03:57 --> Severity: Notice  --> Undefined variable: opciones F:\wamp\www2\bolsa2011\sistema\application\web\views\home.php 327
+ERROR - 2012-07-25 00:03:57 --> Severity: Notice  --> Undefined variable: opciones F:\wamp\www2\bolsa2011\sistema\application\web\views\home.php 442
+ERROR - 2012-07-25 00:03:57 --> Severity: Notice  --> Undefined variable: opciones F:\wamp\www2\bolsa2011\sistema\application\web\views\home.php 478
+ERROR - 2012-07-25 00:03:57 --> Severity: Notice  --> Undefined variable: opciones F:\wamp\www2\bolsa2011\sistema\application\web\views\home.php 496
